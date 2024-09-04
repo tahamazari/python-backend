f533429b20bb85f32edcdaa2bc45a3b2761709de
Mazari <taha.mazari.ext@siemens.com>
2024-09-04 16:28:04 +0500

    base

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from main import getFilteredData
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/process', methods=['POST'])
+def process_data():
+    data = request.json  # Parse the JSON data from the request body
+
+    # Pass the entire data to getFilteredData
+    response = getFilteredData(data)
+
+    # Return the processed data as JSON
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
